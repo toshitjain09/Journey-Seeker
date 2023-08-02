e3020929f731d7e9a50b04d68ad3a0181c6b6fd4
unknown <jaintoshit9@gmail.com>
2023-08-02 19:02:15 +0530

    pushing the code

---
+++ lib/cubit/app_cubit_states.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+import '../model/data_model.dart';
+
+abstract class CubitStates extends Equatable{}
+
+class InitialState extends CubitStates{
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+class WelcomeState extends CubitStates{
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class LoadingState extends CubitStates{
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class LoadedState extends CubitStates{
+  LoadedState(this.places);
+  final List<DataModel> places;
+  @override
+  // TODO: implement props
+  List<Object> get props => [places];
+}
+
+class DetailState extends CubitStates{
+  DetailState(this.places);
+  final DataModel places;
+  @override
+  // TODO: implement props
+  List<Object> get props => [places];
+}
